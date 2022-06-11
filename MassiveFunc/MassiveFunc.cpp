7d0b21e6b13b5a66462a151adae61bdadfb812a1
VforVindetta <gleblarinpo@gmail.com>
2022-06-11 17:32:06 +0300

    Lib for create sorted and half-sorted massive

--- MassiveFunc/MassiveFunc.cpp
+++ MassiveFunc/MassiveFunc.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "MassiveFunc.h"
+#include <random>
+#include <ctime>
+#include <algorithm>
 
 void printM(int *array, int countElem) {
     for (int i = 0; i < countElem; i ++) {
@@ -15,3 +18,18 @@ void GenerateMassive(int *array, int countElem) {
     }
 }
 
+void GenerateMassiveHalf(int *array, int countAll) {
+    GenerateMassive(array, countAll);
+    
+    int * randomHalf = new int[countAll / 2];
+    for (int i = 0; i < countAll / 2; i ++) {
+	*(randomHalf + i) = *(array + countAll / 2 + i);
+    }
+
+    std::shuffle(randomHalf, randomHalf + countAll / 2, std::default_random_engine(unsigned(std::time(0))));
+
+    for (int i = 0; i < countAll / 2; i ++) {
+    	*(array + i + countAll / 2) = *(randomHalf + i);
+    }
+
+}
--- MassiveFunc/MassiveFunc.h
+++ MassiveFunc/MassiveFunc.h
@@ -3,6 +3,6 @@
 
 void printM(int * array, int countElem);
 void GenerateMassive(int *array, int countNumbers);
-
+void GenerateMassiveHalf(int *array, int countAll);
 
 #endif
