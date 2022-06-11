7442700e5528efffbac5fcd3ff34c3df5213a6d8
VforVindetta <gleblarinpo@gmail.com>
2022-06-11 17:31:13 +0300

    Buble sort

--- Bubble/BubbleSort.cpp
+++ Bubble/BubbleSort.cpp
@@ -1,42 +1,25 @@
 #include <algorithm>
 #include <iostream>
-
-void BubbleSort(int *array, int CountElem, int *countSwaps, int *countCompares, bool reversed = false) {
+#include "BubbleSort.h"
+void BubbleSort(int *array, int countElem, int* countSwaps, int* countCompares, bool reversed) {
     bool noSwaps;
-    while (true) {
-	noSwaps = true;
-	if (!reversed) {
-	    for (int i = 0; i < CountElem - 1; i ++) {
-		*countCompares += 1;
-	    	if (*(array + i) > *(array + i + 1)) {
-		    std::swap(*(array + i), *(array + i + 1));
-		    noSwaps = false;
-		    *countSwaps += 1;
-		}
-	    }
+    int sign = (reversed) ? -1: 1;
 
-	    if (noSwaps) {
-		break;
+    while (true) {
+        noSwaps = true;
+        for (int i = 0; i < countElem - 1; i ++) {
+	    *countCompares += 1;
+    	    if (*(array + i) * sign > *(array + i + 1) * sign) {
+	        std::swap(*(array + i), *(array + i + 1));
+	        noSwaps = false;
+	        *countSwaps += 1;
 	    }
-	}
+        }
+
+        if (noSwaps) {
+ 	    break;
 	
-	else {
-	 
-            for (int i = 0; i < CountElem - 1; i++) {
-	        *countCompares += 1;
-		if (*(array + i) < *(array + i + 1)) {
-	            noSwaps = false;
-		    *countSwaps += 1;
-		   
-		    std::swap(*(array+i), *(array+i+1));
-	        }
-             }
-             if (noSwaps) {
-                break;
-            }
-	     
-	}
-	
+        }
     }
 }
  
